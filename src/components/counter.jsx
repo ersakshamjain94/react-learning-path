3a4c450b680aa76df0c53407d2126f185f85d4b6
Saksham Jain <saksjain@amazon.com>
2021-01-22 23:25:18 +0530

    rendering a list dynamically and key attribute 54:57

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,28 +4,30 @@ class Counter extends Component {
   state = {
     count: 1,
     // imageURL:'https://picsum.photos/200'
+    tags: ["tag1", "tag2", "tag3"],
   };
   styles = {
-      fontSize: 10,
-      fontWeight: 'bold'
+    fontSize: 10,
+    fontWeight: "bold",
   };
   render() {
-      // render the className dynamically
-      
     return (
       <div>
-        <span className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
-    getBadgeClasses() {
-        let classes = "badge m-2 ";
-        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
 
   formatCount() {
     const { count } = this.state; //object destructuring
