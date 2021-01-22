f414a841d9981be488bb8a59b334b22efd6d771a
Saksham Jain <saksjain@amazon.com>
2021-01-22 23:33:12 +0530

    conditionally render tags , concept of trucy falsi 1:01

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,34 +4,22 @@ class Counter extends Component {
   state = {
     count: 1,
     // imageURL:'https://picsum.photos/200'
-    tags: ["tag1", "tag2", "tag3"],
+    tags: ["tag1", "tag2", "tag3" ],
   };
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold",
-  };
-  render() {
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    else
+      return (
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
-      </div>
-    );
+      );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state; //object destructuring
-    return count === 0 ? "Zero" : count;
+  render() {
+      {this.state.tags.length === 0 && 'Please create a tag'}
+    return <div>{this.renderTags()}</div>;
   }
 }
 
