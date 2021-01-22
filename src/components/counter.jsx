5b6b97ffdc01983cc1e1dbd5028b627f1ad0e910
Saksham Jain <saksjain@amazon.com>
2021-01-22 23:20:51 +0530

    start for 1:07 back to button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,23 +3,33 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    // imageURL:'https://picsum.photos/200'
-    tags: ["tag1", "tag2", "tag3" ],
   };
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    else
-      return (
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      );
-  }
+
+  styles = {
+      fontSize: 10,
+      fontWeight: 'bold'
+  };
   render() {
-      {this.state.tags.length === 0 && 'Please create a tag'}
-    return <div>{this.renderTags()}</div>;
+      // render the className dynamically
+      
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+        return classes;
+    }
+
+  formatCount() {
+    const { count } = this.state; //object destructuring
+    return count === 0 ? "Zero" : count;
   }
 }
 
