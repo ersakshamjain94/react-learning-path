df26b673d0415fbd8f2f63e49c1e2d4deae4b225
Saksham Jain <saksjain@amazon.com>
2021-01-23 11:06:07 +0530

    lift state from counters component to app component since we want to share data with navbar 2:04:2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import './App.css';
 import React, { Component } from 'react';
 import NavBar from './components/navbar'
 import Counters from './components/counters'
-function App() {
-  return (
-    <React.Fragment>
-    <NavBar/>
-    <main className = "container">
-      <Counters/>
-    </main>
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    console.log("Deletee handler calleed", counterId);
 
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters]; //cloning of the arrayy
+    const index= counters.indexOf(counter);
+    counters[index] = {...counter}
+    counters[index].value++;
+    this.setState({counters});
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters:counters})
+  };
+  render() { 
+    return (
+      <React.Fragment>
+      <NavBar/>
+      <main className = "container">
+        <Counters counters = {this.state.counters} onReset = {this.handleReset} onIncrement = {this.handleIncrement} onDelete = {this.handleDelete}/>
+      </main>
+      </React.Fragment>
+    );
+  }
+}
+ 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,46 +1,19 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-  handleDelete = (counterId) => {
-    console.log("Deletee handler calleed", counterId);
-
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters]; //cloning of the arrayy
-    const index= counters.indexOf(counter);
-    counters[index] = {...counter}
-    counters[index].value++;
-    this.setState({counters});
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({counters:counters})
-  };
+  
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn btn-primary m-2">
+        <button onClick={this.props.onReset} className="btn btn-primary m-2">
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
           />
         ))}
       </div>
