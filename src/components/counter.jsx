3c543704205c7604cf3914c01c8c206bab79de1d
Saksham Jain <saksjain@amazon.com>
2021-01-23 09:24:27 +0530

    passing event handler from parent to child as a prop Component which owns a state should provide methods to modify that statee since state is private we pass the method from parent to child as referencee in props and then leveerage that to update state of parent from child  1:39

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,19 +15,25 @@ class Counter extends Component {
     //if value doesnt exist react will merge the value
     //this.setSTate schedules a call to render the method we do not know when its an async call
   };
+  // Rule of thumb  cComponent that owns a piece of state should be the one modifying it
+  // In our case Components is the component owning piece of state hence it should be one modifying it
+  // we have to raise event from Child to parent Counter component will raise the event, Counters will handle the eevent
+  // we will have a method in Counters component and pass the reference of that method via prop to the Counter component 
+  //Counter component then can use that prop reference of function to change state in counters
+  //onDelete is the name of our prop
   render() {
-    // render the className dynamically
-    // we are passing reference of function in onClick and not calling the function
-    console.log('props',this.props) //props has children
+
     return (
       <div>
-          {this.props.children}; 
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement(1)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button className="btn btn-danger btn-sm m-2" onClick={this.props.onDelete}>
+        Delete
         </button>
       </div>
     );
