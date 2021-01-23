f177993fb2e0eb9b9211e93714de2d08ef69e8e8
Saksham Jain <saksjain@amazon.com>
2021-01-23 09:34:41 +0530

    1:43 handle deleete event, update the state of parrent and row disappears

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,7 @@ class Counter extends Component {
     fontWeight: "bold",
   };
   handleIncrement = (product) => {
-    console.log(product)
+    console.log(product);
     this.setState({ value: this.state.value + 1 }); // react updates the state property and inside that overrides count with new value
     //if value doesnt exist react will merge the value
     //this.setSTate schedules a call to render the method we do not know when its an async call
@@ -18,11 +18,10 @@ class Counter extends Component {
   // Rule of thumb  cComponent that owns a piece of state should be the one modifying it
   // In our case Components is the component owning piece of state hence it should be one modifying it
   // we have to raise event from Child to parent Counter component will raise the event, Counters will handle the eevent
-  // we will have a method in Counters component and pass the reference of that method via prop to the Counter component 
+  // we will have a method in Counters component and pass the reference of that method via prop to the Counter component
   //Counter component then can use that prop reference of function to change state in counters
   //onDelete is the name of our prop
   render() {
-
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -32,8 +31,11 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <button className="btn btn-danger btn-sm m-2" onClick={this.props.onDelete}>
-        Delete
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.id)}
+        >
+          Delete
         </button>
       </div>
     );
