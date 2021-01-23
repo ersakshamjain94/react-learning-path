4beee2e0226ced54621885792f74ad0d7a7f47a6
Saksham Jain <saksjain@amazon.com>
2021-01-23 08:49:10 +0530

    refactor counter to value, concept of props 1:26

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
   };
 
   styles = {
@@ -10,15 +10,15 @@ class Counter extends Component {
     fontWeight: "bold",
   };
   handleIncrement = (product) => {
-    // this.state.count++; // doesnt work
     console.log(product)
-    this.setState({ count: this.state.count + 1 }); // react updates the state property and inside that overrides count with new value
+    this.setState({ value: this.state.value + 1 }); // react updates the state property and inside that overrides count with new value
     //if value doesnt exist react will merge the value
     //this.setSTate schedules a call to render the method we do not know when its an async call
   };
   render() {
     // render the className dynamically
     // we are passing reference of function in onClick and not calling the function
+    console.log('props',this.props) //each Component has a special property props which has the properties we pass to it
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -33,12 +33,12 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state; //object destructuring
+    const { value: count } = this.state; //object destructuring
     return count === 0 ? "Zero" : count;
   }
 }
