9cdef5d4542eed820e5eb90a170f8234ee6ee247
Saksham Jain <saksjain@amazon.com>
2021-01-23 10:20:28 +0530

    Reset button implementation but single source of truth not present
    hence reset button doesnt work

---
+++ src/components/counters.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    console.log("Deletee handler calleed", counterId);
+
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters:counters})
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleReset} className="btn btn-primary m-2">
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
